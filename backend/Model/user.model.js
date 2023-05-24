233d491c3caa28cbedfd043ac355d09b901fad6f
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-05-24 20:09:09 +0530

    login and registration schema create

---
+++ backend/Model/user.model.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  accessToken: { type: String, required: true },
+  role: { type: String, enum: ["customer", "banker"], required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+let UserLogin = mongoose.model("User", userSchema);
+module.exports = UserLogin;
