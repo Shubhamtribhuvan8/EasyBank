f7f6ffca95cbef0654c846565453b843fa297160
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-05-24 20:17:22 +0530

    making controller schema and model

--- backend/Controllers/router.js
+++ backend/Controllers/router.js
@@ -1,2 +1,22 @@
-const express = require("express");
-const router = express.Router();
+const experess = require("express");
+const routerss = experess.Router();
+const UserLogin = require("../Model/user.model");
+
+routerss.post("/register", async (req, res) => {
+  try {
+    const userDatafinal = await UserLogin.find();
+    return res.status(200).send(userDatafinal);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+routersss.post("/login", async (req, res) => {
+  try {
+    const userDatafinal = await UserLogin.create(req.body);
+    return res.status(200).send(userDatafinal);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
+
+export default routersss;
