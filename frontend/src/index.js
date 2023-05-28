b4f9f60fa023a60ff746c6d4a30ceaa123bf4ae7
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-05-28 07:07:06 +0530

    index.js and reactdom changes for rendering

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-// import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+
+ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById("root")
 );
