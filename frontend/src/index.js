feea2e81a38fb08c9879df95a65a8c0342d83a5f
Shubham Tribhuvan <shubhamtribhuvan8@gmail.com>
2023-05-28 11:18:43 +0530

    added all pakages deployement failed

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createPortal } from "react-dom"; // Import createPortal from react-dom
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import "react-toastify/dist/ReactToastify.css";
 
-const portalRoot = document.getElementById("portal-root"); // Define a root element for the portal
-
-const AppWithPortal = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
 ReactDOM.render(
   <React.StrictMode>
-    {/* Render the portal using createPortal */}
-    {createPortal(AppWithPortal, portalRoot)}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
